139d0b4e9c9f96f63d3ed8be5e2ed40759b9717d
Matvei Rushentsev <matvey.rushentsev@gmail.com>
2025-04-09 16:38:55 +0300

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,20 @@
+let
+  pkgs = import <nixpkgs> {};
+in pkgs.mkShell {
+  packages = [
+    pkgs.python313
+    pkgs.uv
+  ];
+  shellHook = ''
+    export VENV_DIR=.venv
+    if [ ! -d "$VENV_DIR" ]; then
+      python -m venv $VENV_DIR
+    fi
+    
+    source $VENV_DIR/bin/activate
+
+    echo "Python version: $(python --version)"
+    echo "Python executable: $(which python)"
+    uv sync
+  '';
+}
