88c45b0430a5cbbe5c55cabdb57e4e9b4f57acbd
Matvei Rushentsev <matvey.rushentsev@gmail.com>
2025-04-10 20:43:48 +0300

    Add script for default ingredients

---
+++ scripts/load_default_ingredients.py
@@ -0,0 +1,35 @@
+'''
+Loads default ingredients into database from assets file.
+'''
+import json
+
+from sqlmodel import Session, insert
+from app.di import connect_to_db
+from db import Ingredient
+from db import SQLModel
+
+
+def transform_to_db_model(json_model):
+    return Ingredient.model_construct(
+        **json_model, id=json_model['_id']
+    ).model_dump()
+
+
+def main():
+    with open('assets/ingredients.json') as f:
+        data = json.load(f)
+
+    ingredients = [
+        transform_to_db_model(ingredient) for ingredient in data['data']
+    ]
+    
+    engine = connect_to_db()
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        session.exec(insert(Ingredient).values(ingredients))
+        session.commit()
+
+
+if __name__=="__main__":
+    main()
+    
