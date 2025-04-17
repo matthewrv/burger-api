7cf7231ee6556befd9a65ef615bda6f8f3806ff6
Matvei Rushentsev <matvey.rushentsev@gmail.com>
2025-04-17 17:29:46 +0300

    Update pleh script

--- pleh.sh
+++ pleh.sh
@@ -3,31 +3,35 @@
 # pleh.sh - It is like "help", but backwards. Contains shortcuts for useful commands.
 
 COMMAND="$1";
+shift 1
+
 PYTHONPATH=$PWD:$PYTHONPATH
 
 case $COMMAND in
     "format")
         echo "Formatting code..."
         ruff format;
-        ruff check --select I --fix;
+        ruff check --select I --fix
         ;;
 
     "run")
         echo "Executing script..."
-        python $2
+        python $@
         ;;
         
     "test")
         echo "Running tests..."
-        pytest
+        pytest $@
         ;;
 
     *)
-        echo "Usage: pleh.sh <command>"
+        echo "Usage: pleh.sh <command> [OPTIONS]"
         echo
         echo pleh.sh - It is like "help", but backwards. Contains shortcuts for useful commands.
         echo 
         echo "Available commands:"
         echo "  format - Format code using ruff"
+        echo "  run    - Run python script with provided options"
+        echo "  test   - Run pytest with provided options"
         ;;
 esac
