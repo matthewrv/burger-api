f53de804178c72b011ce3800c47b1c96ca5368ce
Matvei Rushentsev <matvey.rushentsev@gmail.com>
2025-04-26 10:39:03 +0300

    Add login tests

--- tests/api/auth/snapshots/snap_test_login.py
+++ tests/api/auth/snapshots/snap_test_login.py
@@ -6,4 +6,10 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots["test_login 1"] = {"email": "test@example.com", "name": "test"}
+snapshots["test_login_happy_path 1"] = {"email": "test@example.com", "name": "test"}
+
+snapshots["test_login_invalid_email 1"] = {"detail": "Incorrect username or password"}
+
+snapshots["test_login_invalid_password 1"] = {
+    "detail": "Incorrect username or password"
+}
--- tests/api/auth/test_login.py
+++ tests/api/auth/test_login.py
@@ -4,7 +4,7 @@ from snapshottest.pytest import PyTestSnapshotTest
 from tests.conftest import SampleUser
 
 
-def test_login(
+def test_login_happy_path(
     client: TestClient, test_user: SampleUser, snapshot: PyTestSnapshotTest
 ) -> None:
     response = client.post(
@@ -15,3 +15,25 @@ def test_login(
     response_json = response.json()
     assert response_json.keys() == {"refreshToken", "accessToken", "user", "success"}
     snapshot.assert_match(response_json["user"])
+
+
+def test_login_invalid_password(
+    client: TestClient, test_user: SampleUser, snapshot: PyTestSnapshotTest
+) -> None:
+    response = client.post(
+        "/api/auth/login",
+        json={"email": test_user.email, "password": "not_actual_password"},
+    )
+    assert response.status_code == 400
+    snapshot.assert_match(response.json())
+
+
+def test_login_invalid_email(
+    client: TestClient, test_user: SampleUser, snapshot: PyTestSnapshotTest
+) -> None:
+    response = client.post(
+        "/api/auth/login",
+        json={"email": "not" + test_user.email, "password": test_user.password},
+    )
+    assert response.status_code == 400
+    snapshot.assert_match(response.json())
