6f9cb6b975b40c2cb6800a3b3e2979578d3e7493
Matvei Rushentsev <matvey.rushentsev@gmail.com>
2025-04-29 10:15:37 +0300

    Fix script typings

--- scripts/plot_phout.py
+++ scripts/plot_phout.py
@@ -1,25 +1,26 @@
 import argparse
+import typing
 
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+import plotly.graph_objects as go  # type: ignore[import-untyped]
+from plotly.subplots import make_subplots  # type: ignore[import-untyped]
 
 import pandas as pd  # isort: skip plotly import first for correct numpy import
 
 
-def main():
+def main() -> None:
     args = parse_args()
     df = read_phout(args.phout_file)
     rps, status_codes, timings = prepare_data(df)
     create_plots(rps, status_codes, timings)
 
 
-def parse_args():
+def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("phout_file")
     return parser.parse_args()
 
 
-def read_phout(file_name):
+def read_phout(file_name: str) -> pd.DataFrame:
     columns = [
         "time",
         "tag",
@@ -44,7 +45,7 @@ def read_phout(file_name):
 NETWORK_ERROR_CODE = 999
 
 
-def prepare_data(df):
+def prepare_data(df: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     status_codes = pd.DataFrame({"proto_code": df["proto_code"]})
     status_codes[df["net_code"] != 0] = NETWORK_ERROR_CODE
 
@@ -57,20 +58,23 @@ def prepare_data(df):
     return rps, status_codes, timings
 
 
-def create_plots(rps: pd.DataFrame, status_codes: pd.DataFrame, timings: pd.DataFrame):
+def create_plots(
+    rps: pd.DataFrame, status_codes: pd.DataFrame, timings: pd.DataFrame
+) -> None:
     fig = make_subplots(rows=2, shared_xaxes=True)
 
     position = {"row": 1, "col": 1}
     quantiles = [0.5, 0.9, 0.95, 0.99, 0.995, 1.0]
     timings_quantiles = timings.resample("1s").quantile(quantiles)
-    colors = ["red", "orange", "gold", "lightgreen", "darkgreen", "grey"]
+    timings_colors = ["red", "orange", "gold", "lightgreen", "darkgreen", "grey"]
 
     quantiles.reverse()
-    colors.reverse()
-    for color, quantile in zip(colors, quantiles):
+    timings_colors.reverse()
+    for color, quantile in zip(timings_colors, quantiles):
         data = timings_quantiles.loc[(slice(None), quantile), :]
+        index = typing.cast(pd.MultiIndex, data.index)
         scatter = go.Scatter(
-            x=data.index.levels[0],
+            x=index.levels[0],
             y=data["interval_real"],
             mode="lines",
             name=f"{quantile * 100} %",
@@ -86,13 +90,13 @@ def create_plots(rps: pd.DataFrame, status_codes: pd.DataFrame, timings: pd.Data
     fig.update_yaxes(type="log", **position)
 
     position = {"row": 2, "col": 1}
-    colors = {
+    status_colors = {
         500: "red",
         200: "green",
         NETWORK_ERROR_CODE: "grey",
     }
-
-    for proto_code in status_codes.index.levels[0]:
+    index = typing.cast(pd.MultiIndex, status_codes.index)
+    for proto_code in index.levels[0]:
         values = status_codes.loc[proto_code]["proto_code"]
         obj = go.Scatter(
             x=values.index,
@@ -103,8 +107,8 @@ def create_plots(rps: pd.DataFrame, status_codes: pd.DataFrame, timings: pd.Data
             legendgroup="legend2",
             legendgrouptitle={"text": "Response statuses"},
             stackgroup="one",
-            fillcolor=colors.get(proto_code, None),
-            line_color=colors.get(proto_code, None),
+            fillcolor=status_colors.get(proto_code, None),
+            line_color=status_colors.get(proto_code, None),
         )
         fig.add_trace(obj, **position)
 
