cbef6488d5652570199bcdb432d16383aa61bc76
Matvei Rushentsev <matvey.rushentsev@gmail.com>
2025-05-18 09:22:18 +0300

    Fix mypy errors

--- app/use_cases/order_notifications/public.py
+++ app/use_cases/order_notifications/public.py
@@ -1,8 +1,9 @@
+from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
 from typing import Annotated
 
 import aio_pika
-from fastapi import Depends, Request, WebSocket
+from fastapi import Depends, FastAPI, Request, WebSocket
 
 from app.repo.orders import OrderFull
 
@@ -11,7 +12,9 @@ from .rabbitmq import OrderConsumer, OrderPublisher
 
 
 @asynccontextmanager
-async def lifespan(app, connection: aio_pika.Connection):
+async def lifespan(
+    app: FastAPI, connection: aio_pika.Connection
+) -> AsyncGenerator[None, None]:
     notifications_manager = InMemoryOrderNotificationManager()
 
     # start rabbitmq consumer and publisher
@@ -35,18 +38,19 @@ class NotificationsUseCase:
         self._in_process_manager = notifications_manager
         self._inter_proccess_publisher = order_publisher
 
-    async def pub(self, order: OrderFull):
+    async def pub(self, order: OrderFull) -> None:
         await self._inter_proccess_publisher.publish_order(order)
 
-    def sub(self, subscriber: OrderSubscriber):
+    def sub(self, subscriber: OrderSubscriber) -> None:
         self._in_process_manager.sub(subscriber)
 
-    def unsub(self, subscriber: OrderSubscriber):
+    def unsub(self, subscriber: OrderSubscriber) -> None:
         self._in_process_manager.unsub(subscriber)
 
 
 async def get_notifications_use_case(
-    request: Request = None, ws: WebSocket = None
+    request: Request = None,  # type: ignore[assignment]
+    ws: WebSocket = None,  # type: ignore[assignment]
 ) -> NotificationsUseCase:
     if request:
         return NotificationsUseCase(
@@ -54,10 +58,13 @@ async def get_notifications_use_case(
             request.app.state.order_publisher,
         )
 
-    return NotificationsUseCase(
-        ws.app.state.notification_manager,
-        ws.app.state.order_publisher,
-    )
+    if ws:
+        return NotificationsUseCase(
+            ws.app.state.notification_manager,
+            ws.app.state.order_publisher,
+        )
+
+    raise RuntimeError("This should never have happened")
 
 
 NotificationDep = Annotated[NotificationsUseCase, Depends(get_notifications_use_case)]
